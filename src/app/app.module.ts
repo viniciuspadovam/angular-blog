277ce7ecb89510be70b37fd52f76da14aef41a56
viniciuspadovam <padovam3373@gmail.com>
2023-05-16 21:40:24 -0300

    adicionado componentes e feito o menu-title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    SmallCardComponent,
+    BigCardComponent
   ],
   imports: [
     BrowserModule
