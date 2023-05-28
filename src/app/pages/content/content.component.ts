b4759d857d47a2eeaf3abe9e1f60f7e03e294534
viniciuspadovam <padovam3373@gmail.com>
2023-05-28 15:50:09 -0300

    estilizacao do content component

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+  @Input() photoCover: string = 'https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png';
+  @Input() title: string = 'aaaaaa';
+  @Input() description: string = 'lorem ipsum dolor sit amet, consectetur adipiscing elit sed diam';
 
   constructor() { }
 
